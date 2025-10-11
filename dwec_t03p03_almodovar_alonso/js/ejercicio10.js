31a12df993f405ada06d1e52324215b8f9321b4f
Alonso Almodóvar <alonsoalmodovar09@gmail.com>
2025-10-11 19:52:13 +0200

    Ejercicio 10-11

--- dwec_t03p03_almodovar_alonso/js/ejercicio10.js
+++ dwec_t03p03_almodovar_alonso/js/ejercicio10.js
@@ -1,1 +1,233 @@
-console.log("T03 - Ejercicio 0X");
+console.log("T03 - Ejercicio 10");
+let opcion = -1;
+let esValido = true;
+
+const arrayLiga = [
+    ["Equipo", "PTS", "PJ", "PG", "PE", "PP"],
+    ["Levante", undefined, undefined, 13, 1, 0],
+    ["Málaga", undefined, undefined, 12, 1, 1],
+    ["Eibar", undefined, undefined, 11, 1, 2],
+    ["Córdoba C.F.", undefined, undefined, 8, 3, 3],
+];
+
+rellenarMatriz(arrayLiga);
+
+do {
+    do {
+        opcion = parseInt(
+            prompt(
+                "¿Cuál de estas acciones quieres hacer? \n 1. Saber el líder \n 2. Equipo con más partidos perdidos \n 3. Equipo con más partidos ganados \n 4. Añadir Equipo \n 5. Introducir Jornada \n 6. Ordenar clasificación \n 0. Salir"
+            )
+        );
+        if (!Number.isInteger(opcion) || opcion < 0 || opcion > 6) {
+            alert("Tiene que ser un número entero positivo entre 0 y 6");
+            esValido = false;
+        } else {
+            esValido = true;
+        }
+    } while (!esValido);
+
+    switch (opcion) {
+        case 0:
+            console.log("¡Adiós!");
+            break;
+        case 1:
+            let lider = saberLider(arrayLiga);
+            console.log(`El líder de la Liga es: ${lider}`);
+            break;
+        case 2:
+            let masPerdidos = saberEquipoConMasPerdidos(arrayLiga);
+            console.log(`El equipo con más partidos perdidos es: ${masPerdidos}`);
+            break;
+        case 3:
+            let masGanados = saberEquipoConMasGanados(arrayLiga);
+            console.log(`El equipo con más partidos ganados es: ${masGanados}`);
+            break;
+        case 4:
+            pedirDatos(arrayLiga);
+            break;
+        case 5:
+            introducirJornada(arrayLiga);
+            break;
+        case 6:
+            ordenarClasificacion(arrayLiga);
+            break;
+        default:
+            console.log("¿Qué ha pasado? Opción no debería de valer eso");
+    }
+} while (opcion != 0);
+
+console.table(arrayLiga);
+
+function rellenarMatriz(matriz) {
+    let puntos = (array) => array[3] * 3 + array[4];
+    let partidosJugados = (array) => array[3] + array[4] + array[5];
+
+    for (let i = 1; i < matriz.length; i++) {
+        matriz[i][1] = puntos(matriz[i]);
+        matriz[i][2] = partidosJugados(matriz[i]);
+    }
+}
+
+function saberLider(matriz) {
+    let lider = matriz[1][1];
+    let equipo = matriz[1][0];
+    for (let i = 2; i < matriz.length; i++) {
+        if (lider < matriz[i][1]) {
+            lider = matriz[i][1];
+            equipo = matriz[i][0];
+        }
+    }
+    return equipo;
+}
+
+function saberEquipoConMasPerdidos(matriz) {
+    let masPerdidos = matriz[1][5];
+    let equipo = matriz[1][0];
+    for (let i = 2; i < matriz.length; i++) {
+        if (masPerdidos < matriz[i][5]) {
+            masPerdidos = matriz[i][5];
+            equipo = matriz[i][0];
+        }
+    }
+    return equipo;
+}
+
+function saberEquipoConMasGanados(matriz) {
+    let masGanados = matriz[1][3];
+    let equipo = matriz[1][0];
+    for (let i = 2; i < matriz.length; i++) {
+        if (masGanados < matriz[i][3]) {
+            masGanados = matriz[i][3];
+            equipo = matriz[i][0];
+        }
+    }
+    return equipo;
+}
+
+function pedirDatos(matriz) {
+    let nombreEquipo = "";
+    let nGanados = 0;
+    let nEmpatados = 0;
+    let nPerdidos = 0;
+    let esValido = true;
+
+    do {
+        nombreEquipo = prompt("Dime el nombre del equipo");
+        if (!isNaN(nombreEquipo)) {
+            alert("Tiene que ser el nombre de un equipo");
+            esValido = false;
+        } else {
+            esValido = true;
+        }
+    } while (!esValido);
+
+    do {
+        nGanados = parseInt(
+            prompt(`Dime el número de partidos ganados que tiene el ${nombreEquipo}`)
+        );
+        if (!Number.isInteger(nGanados) || nGanados < 0 || nGanados > 14) {
+            alert(
+                "Tiene que ser un número entero positivo entre 0 y 14. Recuerda que estamos en la jornada 14"
+            );
+            esValido = false;
+        } else {
+            esValido = true;
+        }
+    } while (!esValido);
+
+    do {
+        nEmpatados = parseInt(
+            prompt(
+                `Dime el número de partidos empatados que tiene el ${nombreEquipo}`
+            )
+        );
+        if (!Number.isInteger(nEmpatados) || nEmpatados < 0 || nEmpatados > 14) {
+            alert(
+                "Tiene que ser un número entero positivo entre 0 y 14. Recuerda que estamos en la jornada 14"
+            );
+            esValido = false;
+        } else {
+            esValido = true;
+        }
+    } while (!esValido);
+
+    do {
+        nPerdidos = parseInt(
+            prompt(`Dime el número de partidos perdidos que tiene el ${nombreEquipo}`)
+        );
+        if (!Number.isInteger(nPerdidos) || nPerdidos < 0 || nPerdidos > 14) {
+            alert(
+                "Tiene que ser un número entero positivo entre 0 y 14. Recuerda que estamos en la jornada 14"
+            );
+            esValido = false;
+        } else {
+            esValido = true;
+        }
+    } while (!esValido);
+
+    let suma = nGanados + nPerdidos + nEmpatados;
+    let puntos = nGanados * 3 + nPerdidos;
+
+    let nPartidos = matriz[2][2];
+    if (suma != nPartidos) {
+        console.log(
+            `No puede ser que los partidos jugados del ${nombreEquipo} no sea ${nPartidos}. Anulamos su introducción en la tabla`
+        );
+        // Este script no valdría si hay alguna jornada aplazada
+    } else {
+        return (matriz[matriz.length] = [
+            nombreEquipo,
+            puntos,
+            suma,
+            nGanados,
+            nEmpatados,
+            nPerdidos,
+        ]);
+    }
+}
+
+function introducirJornada(matriz) {
+    matrizTemp = matriz.slice(1);
+    let esValido = true;
+    let resultado = "";
+    matrizTemp.forEach((equipo) => {
+        do {
+            resultado = prompt(
+                `¿Qué ha hecho el ${equipo[0]} en esta jornada? (g, e o p)`
+            );
+            if (resultado != "g" && resultado != "e" && resultado != "p") {
+                alert("Tiene que ser g (ganar), e (empatar) o p (perder)");
+                esValido = false;
+            } else {
+                esValido = true;
+            }
+        } while (!esValido);
+
+        equipo[2]++;
+        switch (resultado) {
+            case "g":
+                equipo[3]++;
+                break;
+            case "e":
+                equipo[4]++;
+                break;
+            case "p":
+                equipo[5]++;
+                break;
+            default:
+                console.log("¿Qué ha pasado? Opción no debería de valer eso");
+        }
+
+        let puntos = equipo[3] * 3 + equipo[4];
+        equipo[2] = puntos;
+    });
+
+    matriz.splice(1, matriz.length - 1, ...matrizTemp);
+}
+
+function ordenarClasificacion(matriz){
+    matrizTemp = matriz.slice(1);
+    matrizTemp.sort(function(a, b){return b[1]-a[1]})
+    matriz.splice(1, matriz.length - 1, ...matrizTemp);
+}
--- dwec_t03p03_almodovar_alonso/js/ejercicio11.js
+++ dwec_t03p03_almodovar_alonso/js/ejercicio11.js
@@ -1,1 +1,227 @@
-console.log("T03 - Ejercicio 0X");
+console.log("T03 - Ejercicio 11");
+const arrayMap = new Map([]);
+let jugador1;
+do {
+    jugador1 = almacenarJugador();
+    if (jugador1 == "Máquina") {
+        alert("El jugador existe");
+    }
+} while (jugador1 == "Máquina");
+const jugadasJugador1 = almacenarJugadas();
+arrayMap.set(jugador1, jugadasJugador1);
+
+let tipoJugador;
+let jugador2;
+do {
+    tipoJugador = parseInt(
+        prompt(
+            "¿Quieres jugar contra la máquina? \n1. Sí \n2. No, voy a jugar con un amigo"
+        )
+    );
+    if (
+        !Number.isInteger(tipoJugador) ||
+        (tipoJugador != 1 && tipoJugador != 2)
+    ) {
+        alert("No puede ser otra cosa entre 1 y 2. Repítelo");
+        esValido = false;
+    } else {
+        esValido = true;
+    }
+} while (!esValido);
+switch (tipoJugador) {
+    case 1:
+        jugador2 = "Máquina";
+        const jugadasJugador2 = almacenarJugadasMaquina();
+        arrayMap.set(jugador2, jugadasJugador2);
+        break;
+    case 2:
+        jugador2 = almacenarJugador();
+        if (!arrayMap.has(jugador2)) {
+            const jugadasJugador2 = almacenarJugadas();
+            arrayMap.set(jugador2, jugadasJugador2);
+        } else {
+            alert("El jugador existe");
+            do {
+                jugador2 = almacenarJugador();
+                if (jugador2 == jugador1) {
+                    alert("El jugador existe");
+                }
+            } while (jugador2 == jugador1);
+        }
+        break;
+    default:
+        console.log("¿Qué ha pasado? Opción no debería de valer eso");
+}
+
+jugar(arrayMap);
+
+
+function almacenarJugador() {
+    let jugador = "";
+    let esValido = true;
+    do {
+        jugador = prompt("Dime el nombre del usuario");
+        if (!jugador || jugador.trim() == "") {
+            alert("No puede estar vacío.");
+            esValido = false;
+        } else {
+            esValido = true;
+        }
+    } while (!esValido);
+    return jugador;
+}
+
+function almacenarJugadas() {
+    const jugadasSet = new Set(["piedra", "papel", "tijera", "lagarto", "spock"]);
+    const jugadas = [];
+    let esValido = true;
+    let jugadaPalabra = "";
+    let jugadaNumero;
+    let introduccion;
+    do {
+        introduccion = parseInt(
+            prompt(
+                "¿Cómo quieres almacenar las jugadas? (piedra, papel, tijera, lagarto, spock) \n1. Usando números en un menú \n2. Escribiendo directamente el texto"
+            )
+        );
+        if (
+            !Number.isInteger(introduccion) ||
+            (introduccion != 1 && introduccion != 2)
+        ) {
+            alert("No puede ser otra cosa entre 1 y 2. Repítelo");
+            esValido = false;
+        } else {
+            esValido = true;
+        }
+    } while (!esValido);
+    for (let i = 0; i < 5; i++) {
+        switch (introduccion) {
+            case 1:
+                do {
+                    jugadaNumero = parseInt(
+                        prompt(`Dime la jugada ${i + 1} que quieras hacer:
+    1. Piedra
+    2. Papel
+    3. Tijera
+    4. Lagarto
+    5. Spock `)
+                    );
+                    if (
+                        !Number.isInteger(jugadaNumero) ||
+                        jugadaNumero < 1 ||
+                        jugadaNumero > 5
+                    ) {
+                        alert("Tiene que ser un número entero positivo entre 1 y 5");
+                        esValido = false;
+                    } else {
+                        esValido = true;
+                    }
+                } while (!esValido);
+                switch (jugadaNumero) {
+                    case 1:
+                        jugadas.push("piedra");
+                        break;
+                    case 2:
+                        jugadas.push("papel");
+                        break;
+                    case 3:
+                        jugadas.push("tijera");
+                        break;
+                    case 4:
+                        jugadas.push("lagarto");
+                        break;
+                    case 5:
+                        jugadas.push("spock");
+                        break;
+                    default:
+                        console.log("¿Qué ha pasado? Opción no debería de valer eso");
+                }
+                break;
+            case 2:
+                do {
+                    jugadaPalabra = prompt(
+                        `Dime la jugada ${i + 1
+                        } que quieras hacer. (piedra, papel, tijera, lagarto, spock)`
+                    );
+                    if (!jugadasSet.has(jugadaPalabra.toLowerCase())) {
+                        alert(
+                            "Inválido. No puedes poner otra cosa que no sea --> piedra, papel, tijera, lagarto, spock"
+                        );
+                        esValido = false;
+                    } else {
+                        esValido = true;
+                    }
+                } while (!esValido);
+                jugadas.push(jugadaPalabra.toLowerCase());
+                break;
+            default:
+                console.log("¿Qué ha pasado? Opción no debería de valer eso");
+        }
+    }
+    return jugadas;
+}
+
+function almacenarJugadasMaquina() {
+    const jugadasPosibles = ["piedra", "papel", "tijera", "lagarto", "spock"];
+    const jugadas = [];
+    let numero;
+    let jugada;
+
+    for (let i = 0; i < 5; i++) {
+        numero = Math.floor((Math.random() *4))
+        jugada = jugadasPosibles[numero];
+        jugadas.push(jugada)
+        
+    }
+    console.log(jugadas)
+    return jugadas
+}
+
+function jugar(arrayMap) {
+    const jugadores = Array.from(arrayMap.keys());
+
+    const jugador1 = jugadores[0];
+    const jugador2 = jugadores[1];
+
+    const jugadas1 = arrayMap.get(jugador1);
+    const jugadas2 = arrayMap.get(jugador2);
+
+    let ganador1 = 0;
+    let ganador2 = 0;
+
+    for (let i = 0; i < jugadas1.length; i++) {
+        const j1 = jugadas1[i];
+        const j2 = jugadas2[i];
+
+        if (j1 == j2) {
+            console.log(`Jugada ${i + 1} es empate`);
+        } else if (
+            (j1 === "tijera" && (j2 === "papel" || j2 === "lagarto")) ||
+            (j1 === "papel" && (j2 === "piedra" || j2 === "spock")) ||
+            (j1 === "piedra" && (j2 === "tijera" || j2 === "lagarto")) ||
+            (j1 === "lagarto" && (j2 === "spock" || j2 === "papel")) ||
+            (j1 === "spock" && (j2 === "tijera" || j2 === "piedra"))
+        ) {
+            console.log(`Jugada ${i + 1} gana ${jugador1}`);
+            ganador1++;
+        } else {
+            console.log(`Jugada ${i + 1} gana ${jugador2}`);
+            ganador2++;
+        }
+    }
+
+    console.log("---");
+    if (ganador1 > ganador2) {
+        console.log(
+            `Ha ganado ${jugador1} siendo el resultado ${ganador1}-${ganador2}`
+        );
+    } else if (ganador1 < ganador2) {
+        console.log(
+            `Ha ganado ${jugador2} siendo el resultado ${ganador2}-${ganador1}`
+        );
+    } else {
+        console.log(
+            `Ha sido un empate siendo el resultado ${ganador1}-${ganador2}`
+        );
+    }
+}
